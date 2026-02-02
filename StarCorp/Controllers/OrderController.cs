8efc02cbcb062d254ea91fb0add7acaa75cac57e
Albin Stenhoff <astenhoff@sqli.com>
2026-02-02 10:49:34 +0100

    Implemented OrderController base.

---
+++ StarCorp/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StarCorp.Data;
+
+namespace StarCorp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IProductDataService _productDataService;
+        private readonly IOrderDataService _orderDataService;
+
+        public OrderController(ILogger<OrderController> logger, IProductDataService productDataService, IOrderDataService orderDataService)
+        {
+            _logger = logger;
+            _productDataService = productDataService;
+            _orderDataService = orderDataService;
+        }
+
+    }
+}
